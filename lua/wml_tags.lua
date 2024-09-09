f1c36b4c4d1fd2f787a43209728d53a4a1e616f2
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2024-09-09 15:55:39 +0600

    Update wml_tags.lua

--- lua/wml_tags.lua
+++ lua/wml_tags.lua
@@ -41,3 +41,25 @@ function wesnoth.wml_actions.give_experience(cfg)
         end
     end    
 end
+
+-- to make code shorter
+local wml_actions = wesnoth.wml_actions
+
+-- [loot]: replacement for mainline LOOT macro
+-- supported parameters:
+-- StandardSideFilter
+-- amount, raises error if not number
+function wml_actions.loot( cfg )
+	local gold_amount = tonumber( cfg.amount ) or wml.error( "Missing or wrong amount= value in [loot]" )
+	local sides = wesnoth.sides.find( cfg )
+	for index, side in ipairs( sides ) do
+		wml_actions.message {
+			side_for = side.side,
+			speaker = "narrator",
+			message = string.format( tostring( _"You retrieve %d pieces of gold." ), gold_amount ),
+			image = "wesnoth-icon.png",
+			sound = "gold.ogg"
+		}
+		side.gold = side.gold + gold_amount
+	end
+end
